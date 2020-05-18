ab25b3802f19d575f3c9fc8d8ea722167948f923
Homyakin <tox.home@ya.ru>
2020-05-18 20:58:41 +0300

    [NF] added generating folders

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,14 @@
 from flask import Flask
+import os
+
+data_dir = 'app/static/data'
+images_dir = 'app/static/images'
+pdf_dir = os.path.join(data_dir, 'pdf')
+txt_dir = os.path.join(data_dir, 'txt')
+norm_dir = os.path.join(data_dir, 'norm')
+
+for directory in [data_dir, images_dir, pdf_dir, txt_dir, norm_dir]:
+    if not os.path.exists(directory):
+        os.mkdir(directory)
 
 flask_app = Flask(__name__)
